d24bb0ade56a3d13227d71b9ae76bec4ddad1c48
adonbiju <adonbiju@gmail.com>
2023-03-15 12:03:01 +0530

    In frontend,NewPassword page design 50% has been completed.

--- chat-app-frontend/src/routes/index.js
+++ chat-app-frontend/src/routes/index.js
@@ -36,7 +36,7 @@ export default function Router() {
         {element: <LoginPage />, path: "login"},
         { path: "register", element: <RegisterPage /> },
         { path: "reset-password", element: <ResetPasswordPage /> },
-       
+        { path: "new-password", element: <NewPasswordPage /> },
       ]
     },
     { path: "*", element: <Navigate to="/404" replace /> },
@@ -49,3 +49,4 @@ const Settings = Loadable( lazy(() => import("../pages/dashboard/Settings")),);
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
 const RegisterPage = Loadable(lazy(() => import("../pages/auth/Register")));
 const ResetPasswordPage = Loadable(lazy(() => import("../pages/auth/ResetPassword")));
+const NewPasswordPage = Loadable(lazy(() => import("../pages/auth/NewPassword")));
