1607ae01fe2d3b92b553e543c4adc83af78fa716
adonbiju <adonbiju@gmail.com>
2023-03-18 22:07:58 +0530

    In authController ,register is added

--- chat-app-backend/controllers/authController.js
+++ chat-app-backend/controllers/authController.js
@@ -1,11 +1,56 @@
 const jwt = require("jsonwebtoken");
-const otpGenerator = require("otp-generator");
+
+const filterObj = require("../utils/filterObj");
 
 // Model
 const User = require("../models/user");
 
 // this function will return you jwt token
 const signToken = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET);
+
+exports.register = async (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
+  
+    const filteredBody = filterObj(
+      req.body,
+      "firstName",
+      "lastName",
+      "email",
+      "password"
+    );
+  
+    // check if a verified user with given email exists
+  
+    const existing_user = await User.findOne({ email: email });
+  
+    if (existing_user && existing_user.verified) {
+      // user with this email already exists, Please login
+      res.status(400).json({
+        status: "error",
+        message: "Email already in use, Please login.",
+      });
+    } else if (existing_user) {
+      // if not verified than update prev one
+  
+      await User.findOneAndUpdate({ email: email }, filteredBody, {
+        new: true,
+        validateModifiedOnly: true,
+      });
+  
+      // generate an otp and send to email
+      req.userId = existing_user._id;
+      next();
+    } else {
+      // if user is not created before than create a new one
+      const new_user = await User.create(filteredBody);
+  
+      // generate an otp and send to email
+      req.userId = new_user._id;
+      next();
+    }
+  };
+  
+
 
 // User Login
 exports.login = async (req, res, next) => {
